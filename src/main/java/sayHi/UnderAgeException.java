15c50f0592393228db812a3e8e98b124321434f1
Allison Carr <allisoncarr@Allisons-MacBook-Pro.local>
2024-02-02 16:01:00 -0500

    Project Message

---
+++ src/main/java/sayHi/UnderAgeException.java
@@ -0,0 +1,26 @@
+package sayHi;
+
+public class UnderAgeException extends Exception
+{
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * 
+	 */
+	public UnderAgeException()
+	{
+		super("Parent is too Young");
+		
+	}
+
+	/**
+	 * @param message
+	 */
+	public UnderAgeException(String message)
+	{
+		super(message);
+		
+	}
+
+}
